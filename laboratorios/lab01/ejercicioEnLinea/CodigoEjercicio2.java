ef2571686e6b2dfc41ec281d5304900449fd5b3a
LauraZapata <69049472+LauraZapata@users.noreply.github.com>
2020-08-23 17:41:03 -0500

    Se añaden autores de la clase CodigoEjercicio2

--- laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
+++ laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
@@ -2,7 +2,7 @@
 /**
  * Write a description of class CÃ³digoEjercicio2 here.
  * 
- * @author Laura Katterine Zapata Rendon, Maria Alejandra velez Clavijo
+ * @author Laura Katterine Zapata Rendón, Maria Alejandra Velez Clavijo
  * @version (a version number or a date)
  */
 public class CodigoEjercicio2
