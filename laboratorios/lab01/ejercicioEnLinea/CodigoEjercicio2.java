b0e8f073417dc99166d7838ddfebf190b7a331a4
kattezapata <lkzapatar@eafit.edu.co>
2020-08-23 18:02:02 -0500

    Se añaden version de la clase CodigoEjercicio2

--- laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
+++ laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
@@ -3,7 +3,7 @@
  * Write a description of class CÃ³digoEjercicio2 here.
  * 
  * @author Laura Katterine Zapata Rendón, Maria Alejandra Velez Clavijo
- * @version (a version number or a date)
+ * @version 1
  */
 public class CodigoEjercicio2
 {
