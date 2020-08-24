3e74773cd0005f26502129ee3223a430517de52a
malejav02 <68791969+malejav02@users.noreply.github.com>
2020-08-23 22:24:53 -0500

    Implementación ejercicio 1.2

--- laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
+++ laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
@@ -7,4 +7,17 @@
  */
 public class CodigoEjercicio2
 {
+    public static int mcd(int a){
+        return mcd(2*a, 2);
+    }
+    private static int mcd(int a,int b){
+        if(b==0){ 
+            return a;
+        }else{
+            return mcd(b,a%b); 
+            // O(log n)
+        }
+        
+    }
+    
 }
