70988b3f170ec96edfc3497557b7e2185397780e
malejav02 <68791969+malejav02@users.noreply.github.com>
2020-08-24 12:01:42 -0500

    Ejercicios recursión 1 ( CodingBat ) Ejercicios recursión 2 (CodingBat)

--- laboratorios/lab01/codigo/CodigoEjercicio1.java
+++ laboratorios/lab01/codigo/CodigoEjercicio1.java
@@ -17,6 +17,97 @@ public class CodigoEjercicio1
             return mcd(b,a%b); 
             // O(log n)
         }
-        
     }
+        
+    public int countHi(String str) {
+        return countHi(str, 0);
+  
+    } 
+    private int countHi(String str, int a){
+        if(str.length()==0){
+            return a;
+    } else{
+        if(str.endsWith("hi")){
+            a++;
+            return countHi(str.substring(0, str.length()-2),a);
+        }
+        return countHi(str.substring(0, str.length()-1),a);
+      }
+  
+    }
+    
+    public int bunnyEars2(int bunnies) {
+        int num=0;
+        if(bunnies==0){
+            return num=0;
+        }
+        if(bunnies%2==0){
+            return num= 3 + bunnyEars2(bunnies-1);
+        } 
+        if(bunnies%2!=0){
+            return num = 2 + bunnyEars2(bunnies-1);
+        }     
+        return num;
+    }
+    
+    
+    public int triangle(int rows) {
+        int num=0;
+        if(rows==0){
+            return 0;
+        } 
+        else if(rows==1){
+            return 1;
+        } 
+        else{
+            num= rows+triangle(rows-1);
+        }
+        return num;
+    }
+    
+    
+    public boolean groupSum6(int start, int[] nums, int target) {
+        if(start>= nums.length){
+            return target==0;
+        }
+        else{
+            if( nums[start]==6){
+                return groupSum6(start+1, nums, target-6);
+            }
+        return groupSum6(start+1, nums, target-nums[start]) || groupSum6(start+1, nums, target);
+        }
+    }
+    
+    public boolean groupNoAdj(int start, int[] nums, int target) {
+        if(start>=nums.length){
+            return target==0;
+        }
+        else{
+            return groupNoAdj(start+2, nums, target-nums[start]) || groupNoAdj(start+1, nums, target);
+    
+        }
+    }
+    
+    public boolean split53(int[] nums) {
+        return split53(0, nums, 0, 0);
+    }
+    private boolean split53(int start, int [] nums, int sum5 , int sum3) {
+        if(start>=nums.length){
+            return sum5==sum3;
+        } 
+        else{
+            if(nums[start]%5==0){
+                return split53(start+1, nums, sum5+nums[start], sum3);
+            }
+            else if(nums[start]%3==0){
+                return split53(start+1, nums, sum5, sum3+nums[start]);
+            } 
+            else{
+                return split53(start+1, nums, sum5+nums[start], sum3) || split53(start+1, nums, sum5, sum3+nums[start]);
+            }
+      
+           }
+    }
+
 }
+    
