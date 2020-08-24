c181c809dc43a770c73dbd43eea53ecec4eef286
malejav02 <68791969+malejav02@users.noreply.github.com>
2020-08-23 23:02:31 -0500

    Ejercicios recursión 1 ( CodingBat )

--- laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
+++ laboratorios/lab01/ejercicioEnLinea/CodigoEjercicio2.java
@@ -7,17 +7,50 @@
  */
 public class CodigoEjercicio2
 {
-    public static int mcd(int a){
-        return mcd(2*a, 2);
-    }
-    private static int mcd(int a,int b){
-        if(b==0){ 
+    public int countHi(String str) {
+        return countHi(str, 0);
+  
+    } 
+    private int countHi(String str, int a){
+        if(str.length()==0){
             return a;
-        }else{
-            return mcd(b,a%b); 
-            // O(log n)
+    } else{
+        if(str.endsWith("hi")){
+            a++;
+            return countHi(str.substring(0, str.length()-2),a);
         }
-        
+        return countHi(str.substring(0, str.length()-1),a);
+      }
+  
     }
     
+    public int bunnyEars2(int bunnies) {
+        int num=0;
+        if(bunnies==0){
+            return num=0;
+        }
+        if(bunnies%2==0){
+            return num= 3 + bunnyEars2(bunnies-1);
+        } 
+        if(bunnies%2!=0){
+            return num = 2 + bunnyEars2(bunnies-1);
+        }     
+        return num;
+    }
+    
+    
+    public int triangle(int rows) {
+        int num=0;
+        if(rows==0){
+            return 0;
+        } 
+        else if(rows==1){
+            return 1;
+        } 
+        else{
+            num= rows+triangle(rows-1);
+        }
+        return num;
+    }
+
 }
