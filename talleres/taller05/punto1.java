71ab100d38ca4f882866a100ea0b14b883b5bc4b
malejav02 <68791969+malejav02@users.noreply.github.com>
2020-08-27 19:46:39 -0500

    Algoritmo inertion sort con ciclos y documentación.

--- talleres/taller05/punto1.java
+++ talleres/taller05/punto1.java
@@ -8,7 +8,68 @@
  **/
 
 public class punto1{
-    private static int min, max;
+  
+/**
+     * El método intertionSort sirve como método auxiliar, el cual le pasa el array a que recibe
+     * como parámetro, al método cicloMayor
+     * @param [] a arreglo a ordenar
+     */
+    public static void insertionSort(int[] a){
+        cicloMayor(a);
+    }
+
+    /**
+     * El método cicloMayor es un método que ayuda en el recorrido del arreglo.
+     * Envía al método cicloMenorMayor el arreglo a ordenar y un iterador que será útil para 
+     * recorrer cada uno de los elementos del arreglo; dicho iterador va desde cero, o posición inicial
+     * y hasta la última posición del arreglo
+     * @param [] a arreglo a ordenar
+     */
+
+    public static void cicloMayor(int[] a){
+        for(int i = 0; i < a.length; i++ )
+            cicloMenoraMayor(a, i);
+    }
+
+    /**
+     * El método cicloMenoraMayor es un método que ayuda en el recorrido del arreglo.
+     * Envía al método MenorMayor el arreglo a ordenar y dos iteradores que serán útiles en la indicación y recorrido
+     * cada elemento del arreglo y su antecesor. 
+     * @param [] a arreglo a ordenar
+     * @param int n iterador que servirá como índice 
+     */
+    public static void cicloMenoraMayor(int[] a, int n){
+        for(int j = n; j > 0 ; j--)
+            MenorMayor(a, j, j-1);
+    }
+
+    /**
+     * El método MenorMayor identifica si el elemento en la posición n es mayor o menor al elemento
+     * de la posición n-1.
+     * Si el elemento en la posición n es menor al de la posición n-1 entones se llama al método intercambiar
+     * y se pasa el arreglo y  dichos índices de los elementos a intercambiar
+     * @param [] a arreglo a ordenar
+     * @param int izq Número de índice de un elemento que está en una posición n-1
+     * @param int der Número del índice de un elemento que está en la posición n
+     */
+    public static void MenorMayor(int[] a, int izq, int der){
+        if (a[der] < a[izq])
+            intercambiar(a, izq, der);
+    }
+
+    /**
+     * El método intercambiar es un método que intercambia de posición los los elementos
+     * de tal forma de que queden de menor a mayor
+     * @param [] a arreglo a ordenar
+     * @param int izq Número de índice de un elemento que está en una posición n-1
+     * @param int der Número del índice de un elemento que está en la posición n
+     */
+    public static void intercambiar(int[] a, int izq, int der){
+        int aux = a[izq];
+        a[izq] = a[der];        
+        a[der] = aux;
+    }
+
 
     public static void main(String [] args){
 
@@ -17,57 +78,17 @@ public class punto1{
             for (int i=arr.length-1; i==0; i++){
                 arr[i]= i;
             }
-           long tiempoInicial=System.currentTimeMillis();
-            OrdenarArr(arr);
+            long tiempoInicial=System.currentTimeMillis();
+            insertionSort(arr);
             long tiempoFinal=System.currentTimeMillis();
             System.out.println(tiempoFinal-tiempoInicial);
-        
+
         }
 
     }
-    /**
-     * El método OrdenarArr permite ordenar los elementos de un arreglo de  menor a mayor 
-     * @param array arreglo de enteros 
-     * @return int[] Arreglo de enteros ordenados ascendentemente
-     */
-    public static int[] OrdenarArr(int[] array){
-        return OrdenarArr(array, 0);
-    }
-    /**
-     * El método OrdenarArr es un auxiliar que permite ordenar los elementos de un arreglo de  menor a mayor 
-     * @param array arreglo de enteros 
-     * @param n iterador
-     * @return int[] Arreglo de enteros ordenados ascendentemente
-     */
-    private static int[] OrdenarArr(int[] array, int n ){
-        if(n==1){
-            min=max=array[n];    
-        }
-
-        if(n>array.length-1){
-            return array;
-        }
-        else{
-            if(n > 0 && array[n]<array[n-1]){
-                min= array[n];
-                max= array[n-1];
-                array[n]=max;
-                array[n-1]=min;
-                return OrdenarArr(array, n-1);
-                // Complejidad:
-                // T(n)= c_1 + T(n-1), donde c_1=19
-                // T(n)= c1 n + c_1
-                // O(c1 n + c_1) (Simetría)
-                // O(c1 n ) (Regla de la suma)
-                // O(n) (Regla del producto)
-                // O(n) Complejidad lineal
 
 
-
-            }
-            return OrdenarArr(array, n+1);
-        }
-
-    }
+    
+    
 
 }
