4fcf8e828d39af7c088228e30e7a83058d4f2d49
kattezapata <68928386+kattezapata@users.noreply.github.com>
2020-08-27 21:55:40 -0500

    Add files via upload

---
+++ talleres/taller05/Punto2.java
@@ -0,0 +1,31 @@
+
+/**
+ * La clase Punto2 tiene como objetivo implementar un algoritmo
+ * que calcule la suma de los elementos de un arreglo utilizando ciclos
+ * 
+ * @author Laura Katterine Zapata Rendon, Maria Alejandra Velez Clavijo
+ * @version 1
+ */
+public class Punto2
+{
+    public static void main(String [] args){
+      
+     for (int n = 10000000; n <= 200000000; n = n + 10000000) {
+        long ti = System.currentTimeMillis();
+        sumaArreglo(new int[n]);
+        long tf = System.currentTimeMillis();
+        System.out.println(tf - ti);
+     }
+    }
+    
+    public static int sumaArreglo(int [] arreglo){
+        int suma=0; // c1 
+        for (int i=0;i<arreglo.length;i++){ // c2 + c3n
+            suma = suma + arreglo[i]; // c4*n
+        }
+        return suma; // c5
+                     // T(n) = c1 + c2 + c3n + c4n + c5 la complejidad es O(n)
+    }
+                  
+
+}
