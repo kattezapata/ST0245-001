cdab935bdf15917465925398a2e67289c2f05631
malejav02 <68791969+malejav02@users.noreply.github.com>
2020-11-05 22:34:35 -0500

    Ejercicio 2.1

--- laboratorios/lab05/ejercicioEnLinea/Colorear.java
+++ laboratorios/lab05/ejercicioEnLinea/Colorear.java
@@ -8,7 +8,6 @@ import java.util.*;
 
 public class Colorear { 
 
-
     /**
      * Este método retorna true si el grafo se puede colorear
      * @param Grafo[][] matriz de adyacencia del grafo a colorear
@@ -22,26 +21,29 @@ public class Colorear {
         int[] colores= new int[Grafo.length];
         for(int i =0; i<colores.length; i++){
             colores[i]=-1;
-        }
+        } 
+        // n: numero de vertices del grafo
+        // O(n)
+
         colores[cabeza] = 1; 
 
         //crea una cola de vertices
         LinkedList<Integer> queue = new LinkedList<Integer>(); 
-        queue.add(cabeza); 
+        queue.add(cabeza);//O(1) 
 
         // Pasa por todos los vertices en la cola
-        while (!queue.isEmpty()) 
+        while (!queue.isEmpty()) // T(n)=n+c
         { 
 
-            int verticeActual = queue.getFirst(); 
-            queue.pop(); 
+            int verticeActual = queue.getFirst(); //O(1)
+            queue.poll(); // O(1)
 
             // Retorna falso si se conecta a si mismo
             if (Grafo[verticeActual][verticeActual] == 1) 
                 return false;   
 
             // Encuentra los vertices adyacentes no coloreados
-            for (int v = 0; v < colores.length; v++) 
+            for (int v = 0; v < colores.length; v++) // T(n)= n + c
             { 
 
                 // Si hay una conexion entre los vertices u y v y v no esta coloreado
@@ -50,7 +52,7 @@ public class Colorear {
 
                     // Asigna el color contrario al vertice actual
                     colores[v] = 1 - colores[verticeActual]; 
-                    queue.push(v); 
+                    queue.add(v); //O(1)
                 } 
 
                 // Si hay una conexion y tiene los mismos colores
@@ -58,7 +60,9 @@ public class Colorear {
                 colores[verticeActual]) 
                     return false; 
             } 
-        } 
+        } //T(n)= n*n + c
+        //O(n^2)
+        //Donde n es el numero de vertices del grafo
 
         //  si llego hasta acá entonces todos los vertices adyacentes tienen distinto color
         return true; 
